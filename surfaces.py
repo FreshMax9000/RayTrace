0519562896854507764a2bb3641aa08a1647d9cf
TimmyTomate <55536665+TimmyTomate@users.noreply.github.com>
2020-12-20 21:50:38 +0100

    Changed function of surfaces and worked on Raytracer

--- surfaces.py
+++ surfaces.py
@@ -4,10 +4,10 @@ import numpy as np
 
 class Surfaces:
 
-    def __init__(self, *surfacelist):
+    def __init__(self, *surfacetupel):
         """Constructor method
         """
-        self.__sflist = surfacelist
+        self.__sflist = list(surfacetupel)
         
 
     
@@ -18,12 +18,15 @@ class Surfaces:
         
         return self.__sflist
 
-    def checkIfCollisionObj(self, ray):
+
+
+    def getCollisionObject(self, ray):
         #Initialiesieren der Matrixen für die  linalg.solve Funktion
         #Werden initialiesiert, sodass sie in for-Schleife Stück für Stück gesetzt werden können
         #Alle mit float, da sobald ein Wert in array --> alle float, generell keine häufigen Speicherzuweisungen durch diesen Stil
         coeffMatrix = np.array([[0.0,0.0,0.0],[0.0,0.0,0.0],[0.0,0.0,0.0]]) 
         resultMatrix = np.array([0.0,0.0,0.0])
+        smallestMatrix = np.array([0.0,0.0,50000.0])
 
         for surface in self.__sflist: #iterieren mit jeder Fläche, sodass jeder mögliche Schnittpunkt gefunden wird
             for i in range(3):        #Aufbauen der Koeffizientenmatrix und der Ergebnismatirx zur Anwendung der .solve Funktion
@@ -33,18 +36,19 @@ class Surfaces:
                 varMatrix = np.linalg.solve(coeffMatrix,resultMatrix)
             except:                   #Wenn nicht lösbar --> kein Schnittpunkt bzw. parrallel, nächste Fläche wird geprüft -->break
                 break
-                                      #Wenn es über Fläche von geprüfter Oberfläche hinaushgeht oder Ray in falsche Richtung
-                                      # --> kein Schnittpunkt --> break
+                                      
             if varMatrix[0] > 1 or varMatrix[0] < 0 or varMatrix[1] > 1 or varMatrix[1] < 0 or varMatrix[2] <= 0:
                 break
-                                      #Wenn Funktion hier angelangt --> legitimer Schnittpunkt von Ray und irgendeiner Fläche
-                                      #Eine 1 wird zurückgegeben
-            return 1
-                                      #Wenn alle Flächen durchlaufen und keine Schnittpunkt --> 0 zurückgeben
-        return 0
-
+            if varMatrix[2] < smallestMatrix[2]:
+                smallestMatrix = varMatrix
+                associatedSurface = surface
+        if smallestMatrix[2] != 50000.0:
+            for i in range(0,2):
+                resultMatrix[i] = associatedSurface.supVec[i]+associatedSurface.dirVec1[i]+associatedSurface.dirVec2[i]
+                return resultMatrix
+        else:
+            return -1
         
-    
 
         
     
