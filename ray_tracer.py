e3fe7ab64387d4a29b6de03d4d36a4eea8634636
Denis Alber <56383734+DenisAlber@users.noreply.github.com>
2020-12-21 16:46:16 +0100

    update ray_tracer.py

--- ray_tracer.py
+++ ray_tracer.py
@@ -11,31 +11,31 @@ from light_source import LightSource
 class RayTracer:
 
     def _init_room(self):
-        leftWall = Surface(np.array([1.25, -0.75, 0]), np.array([0, 1.5, 0]), np.array([0, 0, 2.5]), np.array([0, 0, 0]), np.array([0, 0, 0]), np.array([0, 0, 0]), 1, 1)
-        rightWall = Surface(np.array([-1.25, -0.75, 0]), np.array([0, 1.5, 0]), np.array([0, 0, 2.5]), np.array([0, 0, 0]), np.array([0, 0, 0]), np.array([0, 0, 0]), 1, 1)
-        bottomWall = Surface(np.array([1.25, -0.75, 0]), np.array([0, 0, 2.5]), np.array([-2.5, 0, 0]), np.array([0, 0, 0]), np.array([0, 0, 0]), np.array([0, 0, 0]), 1, 1)
-        topWall = Surface(np.array([1.25, 0.75, 0]), np.array([-2.5, 0, 0]), np.array([0, 0, 2.5]), np.array([0, 0, 0]), np.array([0, 0, 0]), np.array([0, 0, 0]), 1, 1)
-        backWall = Surface(np.array([1.25, -0.75, 2.5]), np.array([-2.5, 0, 0]), np.array([0, 1.5, 0]), np.array([0, 0, 0]), np.array([0, 0, 0]), np.array([0, 0, 0]), 1, 1)
+        leftWall = Surface(np.array([1.25, -0.75, 0]), np.array([0, 1.5, 0]), np.array([0, 0, 2.5]), np.array([1, 0, 0]), np.array([0, 0, 0]), np.array([0, 0, 0]), 1, 1)
+        rightWall = Surface(np.array([-1.25, -0.75, 0]), np.array([0, 1.5, 0]), np.array([0, 0, 2.5]), np.array([0.4, 0.8, 1]), np.array([0, 0, 0]), np.array([0, 0, 0]), 1, 1)
+        bottomWall = Surface(np.array([1.25, -0.75, 0]), np.array([0, 0, 2.5]), np.array([-2.5, 0, 0]), np.array([1, 1, 1]), np.array([0, 0, 0]), np.array([0, 0, 0]), 1, 1)
+        topWall = Surface(np.array([1.25, 0.75, 0]), np.array([-2.5, 0, 0]), np.array([0, 0, 2.5]), np.array([1, 1, 1]), np.array([0, 0, 0]), np.array([0, 0, 0]), 1, 1)
+        backWall = Surface(np.array([1.25, -0.75, 2.5]), np.array([-2.5, 0, 0]), np.array([0, 1.5, 0]), np.array([1, 1, 1]), np.array([0, 0, 0]), np.array([0, 0, 0]), 1, 1)
         room = Surfaces(leftWall, rightWall, bottomWall, topWall, backWall)
         return room
 
     def _init_cuboid(self):
-        frontRighttCuboid = Surface(np.array([-0.6, -0.75, 0.5]), np.array([0, 1, 0]), np.array([-0.25, 0, 0.25]), np.array([0, 0, 0]), np.array([0, 0, 0]), np.array([0, 0, 0]), 1, 1)
-        frontLeftCuboid = Surface(np.array([-0.6, -0.75, 0.5]), np.array([0, 1, 0]), np.array([0.25, 0, 0.25]), np.array([0, 0, 0]), np.array([0, 0, 0]), np.array([0, 0, 0]), 1, 1)
-        backLeftCuboid = Surface(np.array([-0.6, -0.75, 1]), np.array([0, 1, 0]), np.array([0.25, 0, -0.25]), np.array([0, 0, 0]), np.array([0, 0, 0]), np.array([0, 0, 0]), 1, 1)
-        backRightCuboid = Surface(np.array([-0.6, -0.75, 1]), np.array([0, 1, 0]), np.array([-0.25, 0, -0.25]), np.array([0, 0, 0]), np.array([0, 0, 0]), np.array([0, 0, 0]), 1, 1)
-        bottomCuboid = Surface(np.array([-0.6, -0.75, 0.5]), np.array([-0.25, 0, 0.25]), np.array([0.25, 0, 0.25]), np.array([0, 0, 0]), np.array([0, 0, 0]), np.array([0, 0, 0]), 1, 1)
-        topCuboid = Surface(np.array([-0.6, 0.25, 0.5]), np.array([-0.25, 0, 0.25]), np.array([0.25, 0, 0.25]), np.array([0, 0, 0]), np.array([0, 0, 0]), np.array([0, 0, 0]), 1, 1)
+        frontRighttCuboid = Surface(np.array([-0.6, -0.75, 0.5]), np.array([0, 1, 0]), np.array([-0.25, 0, 0.25]), np.array([1, 1, 1]), np.array([0, 0, 0]), np.array([0, 0, 0]), 1, 1)
+        frontLeftCuboid = Surface(np.array([-0.6, -0.75, 0.5]), np.array([0, 1, 0]), np.array([0.25, 0, 0.25]), np.array([1, 1, 1]), np.array([0, 0, 0]), np.array([0, 0, 0]), 1, 1)
+        backLeftCuboid = Surface(np.array([-0.6, -0.75, 1]), np.array([0, 1, 0]), np.array([0.25, 0, -0.25]), np.array([1, 1, 1]), np.array([0, 0, 0]), np.array([0, 0, 0]), 1, 1)
+        backRightCuboid = Surface(np.array([-0.6, -0.75, 1]), np.array([0, 1, 0]), np.array([-0.25, 0, -0.25]), np.array([1, 1, 1]), np.array([0, 0, 0]), np.array([0, 0, 0]), 1, 1)
+        bottomCuboid = Surface(np.array([-0.6, -0.75, 0.5]), np.array([-0.25, 0, 0.25]), np.array([0.25, 0, 0.25]), np.array([1, 1, 1]), np.array([0, 0, 0]), np.array([0, 0, 0]), 1, 1)
+        topCuboid = Surface(np.array([-0.6, 0.25, 0.5]), np.array([-0.25, 0, 0.25]), np.array([0.25, 0, 0.25]), np.array([1, 1, 1]), np.array([0, 0, 0]), np.array([0, 0, 0]), 1, 1)
         cuboid = Surfaces(frontRighttCuboid, frontLeftCuboid, backLeftCuboid, backRightCuboid, bottomCuboid, topCuboid)
         return cuboid
 
     def _init_cube(self):
-        frontCube = Surface(np.array([0.75, -0.75, 0.5]), np.array([0, 0.5, 0]), np.array([-0.5, 0, 0]), np.array([0, 0, 0]), np.array([0, 0, 0]), np.array([0, 0, 0]), 1, 1)
-        leftCube = Surface(np.array([0.75, -0.75, 0.5]), np.array([0, 0.5, 0]), np.array([0, 0, 0.5]), np.array([0, 0, 0]), np.array([0, 0, 0]), np.array([0, 0, 0]), 1, 1)
-        bottomCube = Surface(np.array([0.75, -0.75, 0.5]), np.array([-0.5, 0, 0]), np.array([0, 0, 0.5]), np.array([0, 0, 0]), np.array([0, 0, 0]), np.array([0, 0, 0]), 1, 1)
-        backCube = Surface(np.array([0.25, -0.25, 1]), np.array([0.5, 0, 0]), np.array([0, -0.5, 0]), np.array([0, 0, 0]), np.array([0, 0, 0]), np.array([0, 0, 0]), 1, 1)
-        rightCube = Surface(np.array([0.25, -0.25, 1]), np.array([0, 0, -0.5]), np.array([0, -0.5, 0]), np.array([0, 0, 0]), np.array([0, 0, 0]), np.array([0, 0, 0]), 1, 1)
-        topCube = Surface(np.array([0.25, -0.25, 1]), np.array([0, 0, -0.5]), np.array([0.5, 0, 0]), np.array([0, 0, 0]), np.array([0, 0, 0]), np.array([0, 0, 0]), 1, 1)
+        frontCube = Surface(np.array([0.75, -0.75, 0.5]), np.array([0, 0.5, 0]), np.array([-0.5, 0, 0]), np.array([1, 1, 1]), np.array([0, 0, 0]), np.array([0, 0, 0]), 1, 1)
+        leftCube = Surface(np.array([0.75, -0.75, 0.5]), np.array([0, 0.5, 0]), np.array([0, 0, 0.5]), np.array([1, 1, 1]), np.array([0, 0, 0]), np.array([0, 0, 0]), 1, 1)
+        bottomCube = Surface(np.array([0.75, -0.75, 0.5]), np.array([-0.5, 0, 0]), np.array([0, 0, 0.5]), np.array([1, 1, 1]), np.array([0, 0, 0]), np.array([0, 0, 0]), 1, 1)
+        backCube = Surface(np.array([0.25, -0.25, 1]), np.array([0.5, 0, 0]), np.array([0, -0.5, 0]), np.array([1, 1, 1]), np.array([0, 0, 0]), np.array([0, 0, 0]), 1, 1)
+        rightCube = Surface(np.array([0.25, -0.25, 1]), np.array([0, 0, -0.5]), np.array([0, -0.5, 0]), np.array([1, 1, 1]), np.array([0, 0, 0]), np.array([0, 0, 0]), 1, 1)
+        topCube = Surface(np.array([0.25, -0.25, 1]), np.array([0, 0, -0.5]), np.array([0.5, 0, 0]), np.array([1, 1, 1]), np.array([0, 0, 0]), np.array([0, 0, 0]), 1, 1)
         cube = Surfaces(frontCube, leftCube, bottomCube, backCube, rightCube, topCube)
         return cube
 
