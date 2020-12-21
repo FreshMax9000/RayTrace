7bd620c6b595136c15b58392b4ed657673c72485
Max <max@ferdinand.fritz.box>
2020-12-21 14:01:33 +0100

    Added main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import time
+
+from ray_tracer import RayTracer
+
+if __name__ == "__main__":
+    startTime = time.time()
+
+    height = 200
+    width = 300
+    rayTracer = RayTracer(height, width)
+
+    for height_px in range(height):
+        for width_px in range(width):
+            rayTracer.traceRays(height_px, width_px)
+    
+    rayTracer.printImage("test_image")
+    print("---Finished---")
+    print("The render took %fs\n"%(time.time() - startTime))
