b41ec3e4b91000603a80500f3080b62088368363
TimmyTomate <55536665+TimmyTomate@users.noreply.github.com>
2020-12-21 11:40:56 +0100

    Update RayTracer.py

--- RayTracer.py
+++ RayTracer.py
@@ -18,7 +18,7 @@ leftWall = Surface(np.array([1.25, -0.75, 0]), np.array([0, 1.5, 0]), np.array([
 rightWall = Surface(np.array([-1.25, -0.75, 0]), np.array([0, 1.5, 0]), np.array([0, 0, 2.5]), np.array([0, 0, 0]), np.array([0, 0, 0]), np.array([0, 0, 0]), 1, 1)
 bottomWall = Surface(np.array([1.25, -0.75, 0]), np.array([0, 0, 3]), np.array([2.5, 0, 0]), np.array([0, 0, 0]), np.array([0, 0, 0]), np.array([0, 0, 0]), 1, 1)
 topWall = Surface(np.array([1.25, 0.75, 0]), np.array([2.5, 0, 0]), np.array([0, 0, 2.5]), np.array([0, 0, 0]), np.array([0, 0, 0]), np.array([0, 0, 0]), 1, 1)
-backWall = Surface(np.array([1.25, -0.75, 2.5]), np.array([2.5, 0, 0]), np.array([0, 1.5, 0]), np.array([0, 0, 0]), np.array([0, 0, 0]), np.array([0, 0, 0]), 1, 1)
+backWall = Surface(np.array([1.25, -0.75, 2.5]), np.array([-2.5, 0, 0]), np.array([0, 1.5, 0]), np.array([0, 0, 0]), np.array([0, 0, 0]), np.array([0, 0, 0]), 1, 1)
 
 room = Surfaces(leftWall, rightWall, bottomWall, topWall, backWall)
 
