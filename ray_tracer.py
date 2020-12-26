b724a523521ff94f339737bd7d13620eb16dfe52
Denis Alber <56383734+DenisAlber@users.noreply.github.com>
2020-12-26 20:26:12 +0100

    Update ray_tracer.py

--- ray_tracer.py
+++ ray_tracer.py
@@ -55,7 +55,7 @@ class RayTracer:
         frontRightCuboidPlane = Plane(np.array([0.6, 0.75, -1]), np.array([0, -1, 0]), np.array([0.25, 0, -0.25]))
         frontLeftCuboidPlane = Plane(np.array([0.35, 0.75, -1.25]), np.array([0, -1, 0]), np.array([0.25, 0, 0.25]))
         backLeftCuboidPlane = Plane(np.array([0.6, 0.75, -1.5]), np.array([0, -1, 0]), np.array([-0.25, 0, 0.25]))
-        backRightCuboidPlane = Plane(np.array([0.85, 0.75, -1.5]), np.array([0, -1, 0]), np.array([-0.25, 0, 0.25]))
+        backRightCuboidPlane = Plane(np.array([0.85, 0.75, -1.25]), np.array([0, -1, 0]), np.array([-0.25, 0, 0.25]))
         #bottomCuboidPlane = Plane(np.array([0.6, -0.75, -0.5]), np.array([0.25, 0, 0.25]), np.array([0.25, 0, -0.25]))
         topCuboidPlane = Plane(np.array([0.6, -0.25, -1]), np.array([-0.25, 0, -0.25]), np.array([0.25, 0, -0.25]))
 
