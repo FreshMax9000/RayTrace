ea91be0a5d6d1be6a18edcee99325b5298ab5beb
TimmyTomate <55536665+TimmyTomate@users.noreply.github.com>
2020-12-27 12:39:46 +0100

    Update surfaces.py

--- surfaces.py
+++ surfaces.py
@@ -27,6 +27,7 @@ class Surfaces:
         smallestDistance = 100.0
 
         for surface in self.__sflist: #iterieren mit jeder Fläche, sodass jeder mögliche Schnittpunkt gefunden wird
+            
             for i in range(3):        #Aufbauen der Koeffizientenmatrix und der Ergebnismatirx zur Anwendung der .solve Funktion
                 coeffMatrix[i] = [surface.plane.dirVec1[i],surface.plane.dirVec2[i], 0.0 - ray.normDirection[i]]
                 resultMatrix[i] = ray.origin[i] -surface.plane.supVec[i]
@@ -35,16 +36,54 @@ class Surfaces:
             except:                   #Wenn nicht lösbar --> kein Schnittpunkt bzw. parrallel, nächste Fläche wird geprüft -->break
                 continue
             
+            if varMatrix[0] >= -1.0e-8 and varMatrix[0] < 0.0:
+                varMatrix[0] = 0.0
+            if varMatrix[1] >= -1.0e-8 and varMatrix[1] < 0.0:
+                varMatrix[1] = 0.0
+
             if varMatrix[0] > 1 or varMatrix[0] < 0 or varMatrix[1] > 1 or varMatrix[1] < 0 or varMatrix[2] <= 0:
                 continue
-
+            
             if varMatrix[2] < smallestDistance:
                 smallestDistance = varMatrix[2]
                 associatedSurface = surface
+            """
+            elif varMatrix[2] == smallestDistance:
+                
+                if self.checkIfNewSurfCloser(self.getNearPoint(associatedVarMatrix,associatedSurface),self.getNearPoint(varMatrix,surface), ray.origin):
+                    smallestDistance = varMatrix[2]
+                    associatedSurface = surface
+                    associatedVarMatrix = varMatrix
+            """
         if smallestDistance != 100.0:
             return (associatedSurface, smallestDistance)
         else:
             return (None, smallestDistance)
+
+    def getNearPoint(self,varMatrix, surface):
+        if varMatrix[0] == 0:
+            xVek = 0.01
+        else:
+            xVek = varMatrix[0] * 0.9
+        if varMatrix[1] == 0:
+            yVek = 0.01
+        else:
+            yVek = varMatrix[1] * 0.9
+        returnVek = np.array([0.0,0.0,0.0])
+        for iter in range(3):
+            returnVek[iter] = surface.plane.supVec[iter]+ surface.plane.dirVec1[iter]*xVek+surface.plane.dirVec2[iter]*yVek
+        return returnVek
+
+    def checkIfNewSurfCloser(self,oldSurfPoint, newSurfPoint,origin):
+        oldCombVek = origin - oldSurfPoint
+        newCombVek = origin - newSurfPoint
+        oldLength = np.sqrt(np.square(oldCombVek[0])+np.square(oldCombVek[1])+np.square(oldCombVek[2]))
+        newLength = np.sqrt(np.square(newCombVek[0])+np.square(newCombVek[1])+np.square(newCombVek[2]))
+
+        if newLength < oldLength:
+            return True
+        elif newLength > oldLength:
+            return False
         
 
         
