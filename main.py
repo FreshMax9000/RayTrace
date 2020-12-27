55160f50fcff38268bf3b9aa483122478ffe04a4
TimmyTomate <55536665+TimmyTomate@users.noreply.github.com>
2020-12-27 14:01:24 +0100

    Changed ray_tracer and surfaces

--- main.py
+++ main.py
@@ -5,17 +5,20 @@ from timestopper import TimeStopper
 if __name__ == "__main__":
     timerkek = TimeStopper()
 
-    lines = 720
-    ratio = 16/9   
+    lines = 300
+    ratio = 4/3  
     height = int(lines)
     width = int(lines * ratio)
-    rayTracer = RayTracer(height, width, max_depth=4, randomShadowRays=0, systematicShadowRayRoot=8)
+    rayTracer = RayTracer(height, width, max_depth=4, randomShadowRays=0, systematicShadowRayRoot=1)
 
+
+    #rayTracer.traceRays(479, 454)
+    
     for height_px in range(height):
         for width_px in range(width):
             rayTracer.traceRays(height_px, width_px)
         print("%d / %d . %s"%(height_px, height,timerkek.getPassedTimeString(height_px,height)))
-        
+    
     
     rayTracer.printImage("images/test_image")
     print("---Finished---")
