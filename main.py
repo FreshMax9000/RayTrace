e8ad3dc6468d4cd11f0d0522a5a8be4e70d92e80
TimmyTomate <55536665+TimmyTomate@users.noreply.github.com>
2021-01-10 15:09:50 +0100

    Deleted unnnesasary stuff

--- main.py
+++ main.py
@@ -6,17 +6,16 @@ from timestopper import TimeStopper
 if __name__ == "__main__":
     
 
-    lines = 90
-    ratio = 16/9   
+    lines = 200      #Height of picture
+    ratio = 16/9    #Ratio of pic, width will be calculated 
     height = int(lines)
     width = int(lines * ratio)
-    rayTracer = RayTracer(height, width, max_depth=4, randomShadowRays=0, systematicShadowRayRoot=2, liveDisplay=False)
-
+    rayTracer = RayTracer(height, width, max_depth=4, randomShadowRays=0, systematicShadowRayRoot=2, liveDisplay=True)
+                #max_depth, max number of a rays per pixel, randomShadow donot look good, systematicShadow details of shadows, live display try it 
     timerkek = TimeStopper()
 
 
-    #rayTracer.traceRays(479, 454)
-    
+ 
     for height_px in range(height):
         for width_px in range(width):
             rayTracer.traceRays(height_px, width_px)
